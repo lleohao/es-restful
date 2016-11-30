4ad71a7058c56b13bf6fcad7b5c954f0ea653d5f
lleohao <hzzhouhao1@corp.netease.com>
2016-11-30 19:09:08 +0800

    style: 调整样式

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,29 @@
 module.exports = {
-    "extends": "google"
+    "env": {
+        "es6": true,
+        "node": true,
+        "mocha": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
 };

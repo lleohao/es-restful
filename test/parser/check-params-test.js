e1777963edd0a5c677755524b9c58f65342569d1
lleohao <hzzhouhao1@corp.netease.com>
2016-12-01 19:19:45 +0800

    test(parser): 增加required属性测试用例

--- test/parser/check-params-test.js
+++ test/parser/check-params-test.js
@@ -1,4 +1,4 @@
-/// <reference path="../../lib/@type/parser.d.ts" />
+/// <reference path="../../lib/@types/parser.d.ts" />
 let should = require('should');
 let Parser = require('../../dist/parser').Parser;
 let http = require('http');
@@ -6,16 +6,114 @@ let qs = require('querystring');
 
 
 describe('parser 错误参数检测测试', function () {
-    describe('global trim = false', function () {
-        it('should return requirled error', () => {
+    describe('required test', function () {
+        let server;
 
+        before(() => {
+            server = http.createServer((req, res) => {
+                let parser = new Parser();
+                parser.addParam('name', {
+                    required: true
+                });
+
+                parser.parse(req, res).on('end', (data) => {
+                    res.writeHead(200, {
+                        'Content-type': 'application/json'
+                    });
+                    res.end(JSON.stringify(data));
+                })
+            }).listen(5052);
+        })
+
+        after(() => {
+            server.close();
+        })
+
+
+        it('should return required error', (done) => {
+            http.get({
+                port: 5052
+            }, (res) => {
+                var data = [];
+                res.on('data', chunk => {
+                    data.push(chunk);
+                }).on('end', () => {
+                    data = JSON.parse(data.toString());
+                    data.should.containEql({
+                        error: [{
+                            type: 2,
+                            info: 'name'
+                        }]
+                    })
+                    done();
+                })
+            }).end();
+        })
+
+        it('should return required success', (done) => {
+            http.get({
+                port: 5052,
+                path: '/?name=lleohao'
+            }, (res) => {
+                var data = [];
+                res.on('data', chunk => {
+                    data.push(chunk);
+                }).on('end', () => {
+                    data = JSON.parse(data.toString());
+                    data.should.containEql({
+                        data: {
+                            name: 'lleohao'
+                        }
+                    })
+                    done();
+                })
+            }).end();
         })
     })
 
-    // describe('global trim = true', function () {
-    //     let parser;
-    //     beforeEach(function () {
-    //         parser = new Parser(true);
-    //     })
-    // })
+    describe('nullabled test', function () {
+        let server;
+
+        before(() => {
+            server = http.createServer((req, res) => {
+                let parser = new Parser();
+                parser.addParam('name', {
+                    nullabled: false
+                });
+
+                parser.parse(req, res).on('end', (data) => {
+                    res.writeHead(200, {
+                        'Content-type': 'application/json'
+                    });
+                    res.end(JSON.stringify(data));
+                })
+            }).listen(5052);
+        })
+
+        after(() => {
+            server.close();
+        })
+
+
+        it('should return required error', (done) => {
+            http.get({
+                port: 5052,
+                path: '/?name='
+            }, (res) => {
+                var data = [];
+                res.on('data', chunk => {
+                    data.push(chunk);
+                }).on('end', () => {
+                    data = JSON.parse(data.toString());
+                    data.should.containEql({
+                        error: [{
+                            type: 5,
+                            info: 'name'
+                        }]
+                    })
+                    done();
+                })
+            }).end();
+        })
+    })
 });

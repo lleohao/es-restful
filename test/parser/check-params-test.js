735f3c88f4e3d9738560299e62d745d4fbce1e76
lleohao <hzzhouhao1@corp.netease.com>
2016-12-01 17:33:07 +0800

    refactor: 调整文件

--- test/parser/check-params-test.js
+++ test/parser/check-params-test.js
@@ -1,4 +1,21 @@
- let should = require('should');
- let Parser = require('../../dist/parser').Parser;
- let http = require('http');
- let qs = require('querystring');
+/// <reference path="../../lib/@type/parser.d.ts" />
+let should = require('should');
+let Parser = require('../../dist/parser').Parser;
+let http = require('http');
+let qs = require('querystring');
+
+
+describe('parser 错误参数检测测试', function () {
+    describe('global trim = false', function () {
+        it('should return requirled error', () => {
+
+        })
+    })
+
+    // describe('global trim = true', function () {
+    //     let parser;
+    //     beforeEach(function () {
+    //         parser = new Parser(true);
+    //     })
+    // })
+});

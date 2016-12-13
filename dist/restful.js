82927908c07ff2af6a86a7d0f2bc3e6c544c468e
lleohao <lleohao@hotmail.com>
2016-12-13 15:57:37 +0800

    feat(resuful): 添加主要api

--- dist/restful.js
+++ dist/restful.js
@@ -1,1 +1,30 @@
+"use strict";
+const http_1 = require('http');
+class Api {
+    constructor(port = 5050, hostname = 'localhost') {
+        this.port = port;
+        this.hostname = hostname;
+        this.resourceMap = new Map();
+    }
+    addSource(path, resource) {
+        let resourceMap = this.resourceMap;
+        if (resourceMap.has(path)) {
+            throw SyntaxError(`The path:${path} already exists`);
+        }
+        resourceMap.set(path, resource);
+    }
+    start() {
+        if (this.resourceMap.size === 0) {
+            console.warn('There can not be any proxied resources');
+        }
+        this.server = http_1.createServer((req, res) => {
+        });
+        this.server.listen(this.port, this.hostname);
+    }
+    stop() {
+        if (this.server !== undefined) {
+            this.server.close();
+        }
+    }
+}
 //# sourceMappingURL=restful.js.map
---
+++ lib/restful.ts
@@ -0,0 +1,81 @@
+import { parse } from 'url';
+import { Resource } from './resource';
+import { IncomingMessage, createServer, Server } from 'http';
+
+class Api {
+    private resourceMap: Map<string, Resource>;
+    private port: number;
+    private hostname: string;
+    private server: Server;
+
+    /**
+     * Creates an instance of Api.
+     * 
+     * @param {number} [port=5050]
+     * @param {string} [hostname='localhost']
+     * 
+     * @memberOf Api
+     */
+    constructor(port: number = 5050, hostname: string = 'localhost') {
+        this.port = port;
+        this.hostname = hostname;
+        this.resourceMap = new Map();
+    }
+
+
+    /**
+     * add Resource
+     * 
+     * @param {string} path
+     * @param {Resource} resource
+     * 
+     * @memberOf Api
+     */
+    addSource(path: string, resource: Resource) {
+        let resourceMap = this.resourceMap;
+        if (resourceMap.has(path)) {
+            throw SyntaxError(`The path:${path} already exists.`)
+        }
+        resourceMap.set(path, resource);
+    }
+
+    /**
+     * start server
+     * 
+     * 
+     * @memberOf Api
+     */
+    start() {
+        if (this.resourceMap.size === 0) {
+            console.warn('There can not be any proxied resources')
+        }
+        let server = this.server;
+        let resoureMap = this.resourceMap;
+
+        server = createServer((req, res) => {
+            let path = parse(req.url).pathname;
+            if (resoureMap.has(path)) {
+
+            } else {
+                res.writeHead(404, { 'Content-type': 'application/json' });
+                res.end(JSON.stringify({
+                    code: 404,
+                    message: 'The requested connection does not exist.'
+                }));
+            }
+        });
+        server.listen(this.port, this.hostname);
+    }
+
+    /**
+     * stop server
+     * 
+     * 
+     * @memberOf Api
+     */
+    stop() {
+        if (this.server !== undefined) {
+            this.server.close()
+        }
+    }
+}

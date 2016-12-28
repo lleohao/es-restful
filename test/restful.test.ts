058e24476eef4f8c04d7436a1a3d5efb3a2439f5
lleohao <lleohao@hotmail.com>
2016-12-28 17:19:20 +0800

    test: :tada:完善代码覆盖率测试
    覆盖率95%以上

--- test/restful.test.ts
+++ test/restful.test.ts
@@ -23,6 +23,13 @@ describe('Restful tets', () => {
             });
         })
 
+        it('错误添加resouce', () => {
+            should.throws(() => {
+                api.addSource(1, '/books');
+                api.start();
+            })
+        })
+
         it('添加重复路径 resource 抛出 RestfulError', () => {
             class Todos {
                 get() {
@@ -137,5 +144,111 @@ describe('Restful tets', () => {
                 })
             })
         })
+
+        it('错误路径测试', (done) => {
+            class Books {
+                get({id, page}) {
+                    return {
+                        id: id,
+                        page: page
+                    }
+                }
+            }
+            api.addSource(Books, '/books/<id>/<page>');
+            api.start();
+            get({
+                hostname: 'localhost',
+                port: 5051,
+                path: '/book'
+            }, (res) => {
+                let data = [];
+                res.on('data', (chunk) => {
+                    data.push(chunk)
+                }).on('end', () => {
+                    data = JSON.parse(data.toString());
+                    should(data).be.eql({
+                        code: 404
+                    });
+                    api.stop();
+                    done();
+                })
+            })
+        })
+
+        it('访问未定义方法测试', (done) => {
+            class Books {
+                get({id, page}) {
+                    return {
+                        id: id,
+                        page: page
+                    }
+                }
+            }
+            api.addSource(Books, '/books/<id>/<page>');
+            api.start();
+            let req = request({
+                hostname: 'localhost',
+                port: 5051,
+                path: '/books/1/25',
+                method: 'post'
+            }, (res) => {
+                let data = [];
+                res.on('data', (chunk) => {
+                    data.push(chunk)
+                }).on('end', () => {
+                    data = JSON.parse(data.toString());
+                    should(data).be.eql({
+                        code: 400,
+                        error: {
+                            message: 'post method is undefined.'
+                        }
+                    });
+                    api.stop();
+                    done();
+                })
+            })
+            req.end();
+        })
+
+        it('错误请求方式测试', (done) => {
+            let parser = new Parser();
+            parser.addParam('id');
+            parser.addParam('page');
+            class Books {
+                @addParser(parser)
+                post({id, page}) {
+                    return {
+                        id: id,
+                        page: page
+                    }
+                }
+            }
+            api.addSource(Books, '/books');
+            api.start();
+            let req = request({
+                hostname: 'localhost',
+                port: 5051,
+                path: '/books',
+                method: 'post',
+                headers: {
+                    'Content-Type': 'text/xml'
+                }
+            }, (res) => {
+                let data = [];
+                res.on('data', (chunk) => {
+                    data.push(chunk)
+                }).on('end', () => {
+                    data = JSON.parse(data.toString());
+                    should(data).be.eql({
+                        code: 403,
+                        error: { error: { type: 1, info: 'This request method is not supported' } }
+                    })
+                    api.stop();
+                    done();
+                })
+            })
+            req.write(JSON.stringify({ id: 1, page: 22 }))
+            req.end();
+        })
     })
-})
+}) 

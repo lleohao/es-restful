d8d1b98cfde73f6151bc46ce45836f2b0aae4d90
lleohao <lleohao@hotmail.com>
2017-07-12 15:55:02 +0800

    refactor(remove unused files):

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var restful_1 = require("./restful");
-exports.Restful = restful_1.Restful;
-var resource_1 = require("./resource");
-exports.Resource = resource_1.Resource;
-var parser_1 = require("./parser");
-exports.Parser = parser_1.Parser;
-//# sourceMappingURL=index.js.map

bc7b44265f4ba572866e66ca6f0bbc1466dca311
lleohao <lleohao@hotmail.com>
2017-08-03 20:09:27 +0800

    feat(resource): Remove async options
    Remove async decorator. Becaust in js sync is rare.
    Now resource method function like this:
    
    class R extentds Reource {
    	get(query, args, render) {
    		// query:  data in the route
    		// args:   data in the request
    		// render: when you want return data to client, you can call render function
    		// 	   render(data: any, statusCode: number = 200);
    	}
    
    }

--- src/resource.ts
+++ src/resource.ts
@@ -1,118 +1,17 @@
-import { IncomingMessage } from 'http';
-import { ReqParse, ParsedData } from './params';
+import { ReqParams } from './params';
 
-/**
- * 资源返回值类型
- * 
- * @export
- * @interface ResourceResult
- */
-export interface ResourceResult {
-    /**
-     * 返回数据
-     * 
-     * @type {*}
-     * @memberOf ResourceResult
-     */
-    data: any;
-    /**
-     * http code
-     * 
-     * @type {number}
-     * @default 200
-     * @memberOf ResourceResult
-     */
-    code?: number;
-}
-
-/**
- * Resource 基类
- * 
- * @export
- * @class Resource
- * @extends {EventEmitter}
- */
 export class Resource {
     constructor() {
 
     }
 
-    /**
-     * (装饰器)指定该函数将以异步的方式返回数据
-     * 
-     * @static
-     * @returns
-     * 
-     * @memberOf Resource
-     */
-    static async() {
+    static addParser(params: ReqParams) {
         return function (target: any, propertyKey: string) {
-            target[propertyKey].async = true;
+            target[propertyKey]['params'] = params;
         };
     }
 
-
-    /**
-     * (装饰器)给指定请求绑定参数解析
-     * 
-     * @static
-     * @param {ReqParse} parser
-     * @returns
-     * 
-     * @memberOf Resource
-     */
-    static addParser(parser: ReqParse) {
-        return function (target: any, propertyKey: string) {
-            target[propertyKey].parser = parser;
-        };
-    }
-
-
-    /**
-     * 获取响应数据
-     * 
-     * @param {IncomingMessage} req             请求数据
-     * @param {Object}          routeParams     路由参数
-     * @returns Promise
-     * 
-     * @memberOf Resource
-     */
-    public _getResponse(req: IncomingMessage, routeParams: Object) {
-        let method = req.method.toLowerCase();
-
-        return new Promise((reslove, reject) => {
-            let handle = this[method];
-
-            // 存在当前请求类型的处理函数
-            if (handle) {
-                let parser = <ReqParse>handle.parser;
-                if (parser === undefined) {
-                    if (handle['async']) {
-                        handle(routeParams, reslove)
-                    } else {
-                        reslove(handle(routeParams));
-                    }
-                } else {
-                    // parser.parse(req).once('parseEnd', (data: ParamData) => {
-                    //     if (data.errorData !== undefined) {
-                    //         reject(data.errorData)
-                    //     } else {
-                    //         if (handle['async']) {
-                    //             handle(Object.assign(data, routeParams), reslove)
-                    //         } else {
-                    //             reslove(handle(Object.assign(data, routeParams)));
-                    //         }
-                    //     }
-                    // });
-                }
-            } else {
-                reject({
-                    code: 400,
-                    error: {
-                        message: `${method} method is undefined.`
-                    }
-                })
-            }
-        });
+    public getMethodProcess(method: string) {
+        return this[method.toLowerCase()];
     }
 }

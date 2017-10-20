cc3a3869d4d99f42dba51bc11d5b48495c4695ea
lleohao <lleohao@hotmail.com>
2017-10-20 17:37:09 +0800

    chore(other): Change .gitignore

--- src/resource.ts
+++ src/resource.ts
@@ -1,10 +1,6 @@
 import { ReqParams } from './params';
 
 export class Resource {
-    public options(render) {
-        render();
-    }
-
     public static addParser(params: ReqParams) {
         return (target: any, propertyKey: string) => {
             target[propertyKey].params = params;
